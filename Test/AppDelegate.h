9714eaa1d2e02ece6461c1cf76a905cf02810c7f
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2015-07-15 11:30:23 +0300

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by mac-246 on 15.07.15.
+//  Copyright (c) 2015 mac-246. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by mac-246 on 15.07.15.
+//  Copyright (c) 2015 mac-246. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
